25fd818c3e2485a3c6e3c8f5de1132d541edbaf4
Tom Shlomi <41210998+TomShlomi@users.noreply.github.com>
2023-03-04 20:59:19 -0800

    Initial commit

---
+++ chat.py
@@ -0,0 +1,35 @@
+import openai
+import json
+import argparse
+
+def ask(question : str):
+    with open('context.json', 'r') as fin:
+        context = json.load(fin)
+    try:
+        context.append({"role": "user", "content": question})
+        answer = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=context).choices[0].message.content
+    except openai.error.APIConnectionError:
+        print("No internet")
+        return None
+    context.append({"role": "assistant", "content": answer})
+    print(answer)
+    with open('context.json', 'w') as fout:
+        json.dump(context, fout)
+    return answer
+
+def clear():
+    with open('context.json', 'r') as fin:
+        context = json.load(fin)
+    with open('log.json', 'w') as log:
+        json.dump(context, log)
+    with open('context.json', 'w') as fout:
+        json.dump([{"role": "system", "content": "You are Assistant, a helpful, harmless and honest chatbot."}], fout)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("question", help="Question to ask")
+    parser.add_argument("-c", "--clear", help="Clear context", action="store_true")
+    if parser.parse_args().clear:
+        clear()
+    args = parser.parse_args()
+    ask(args.question)
